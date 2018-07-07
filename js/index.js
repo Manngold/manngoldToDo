2637a2cc184041c9ae97e8748dac0a40191910e9
Manngold <36980767+Manngold@users.noreply.github.com>
2018-07-07 21:58:41 +0900

    practice html, css and js
    add edit and delete icon

--- js/index.js
+++ js/index.js
@@ -10,22 +10,40 @@ const bigBoxCreate = () => {
 
 const smallBoxCreate = () => {
     let getClass = document.getElementsByClassName('todo')[toDoList.length - 1];
-    let giveElement = document.createElement('i');
+    let giveElement = document.createElement('div');
     let divIn = getClass.appendChild(giveElement);
     return divIn;
+};
+const addEdit = () => {
+    let getClass = document.getElementsByClassName('edit')[toDoList.length - 1];
+    let giveElement = document.createElement('i');
+    let editIcon = getClass.appendChild(giveElement);
+    return editIcon;
+};
+
+const addDelete = () => {
+    let getClass = document.getElementsByClassName('delete')[toDoList.length - 1];
+    let giveElement = document.createElement('i');
+    let deleteIcon = getClass.appendChild(giveElement);
+    return deleteIcon;
 };
 
 const createBlock = () => {
     let toDoDiv = bigBoxCreate();
     toDoDiv.className = "todo";
     toDoDiv.innerHTML = toDoList[toDoList.length - 1];
-    let fixDiv = smallBoxCreate();
-    fixDiv.className = "fas fa-edit";
+    let editDiv = smallBoxCreate();
+    editDiv.className = "edit";
+    let editIcon = addEdit();
+    editIcon.className = "fas fa-edit"
     let deleteDiv = smallBoxCreate();
-    deleteDiv.className = "far fa-trash-alt";
+    deleteDiv.className = "delete";
+    let deleteIcon = addDelete();
+    deleteIcon.className = "far fa-trash-alt";
 };
 
 const addList = () => {
+
     toDoList.push(toDo);
     console.log(toDoList);
     createBlock();
@@ -34,6 +52,10 @@ const addList = () => {
 const clickAdd = () => {
     document.getElementById('input_todo').addEventListener("click", addList());
 };
+
+document.getElementsByClassName('edit')[toDoList.length - 1].addEventListener('click', editEvent());
+document.getElementsByClassName('delete')[toDoList.length - 1].addEventListener('click', deleteEvent());
+
 const addToDo = () => {
     toDo = document.getElementById('input_todo').value;
     if (toDo.length == 0){
@@ -52,12 +74,3 @@ const deleteEvent = () => {
     console.log("delete complete");
 };
 
-const editClick = () => {
-    let getClass = document.getElementsByClassName('fas fa-edit')[toDoList.length - 1];
-    getClass.addEventListener('click', editEvent());
-};
-
-const deleteClick = () => {
-    let getClass = document.getElementsByClassName('fas fa-trash-alt')[toDoList.length - 1];
-    getClass.addEventListener('click', deleteEvent());
-};
